909626df1e5d8efd5b41bd6f9e10652449f04f5c
majkeleq <graf3rpl@gmail.com>
2023-07-14 13:43:11 +0200

    Stage 3

--- src/budget/Budget.java
+++ src/budget/Budget.java
@@ -1,14 +1,18 @@
 package budget;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Budget {
     private double balance;
-    private final ArrayList<Purchase> purchases = new ArrayList<>();
+    //private final ArrayList<Purchase> purchases = new ArrayList<>();
+    private final HashMap<Integer, ArrayList<Purchase>> purchases = new HashMap<>();
 
     public Budget() {
     }
+
     public void addIncome(double income) {
         balance += income;
         System.out.println("Income was added\n");
@@ -17,21 +21,38 @@ public class Budget {
     public double getBalance() {
         return balance;
     }
-    public void addPurchase(Scanner sc) {
-        System.out.println("Enter purchase name");
+
+    public void addPurchase(String input, Scanner sc) {
+        ArrayList<Purchase> temp = purchases.getOrDefault(Integer
+                .valueOf(input), new ArrayList<>());
+        temp.add(getNewPurchase(sc));
+        purchases.put(Integer.valueOf(input), temp);
+        System.out.print("Purchase was added!\n\n");
+    }
+
+
+    private Purchase getNewPurchase(Scanner sc) {
+        System.out.println("\nEnter purchase name");
         String name = sc.nextLine();
         System.out.println("Enter its price:");
         double price = Double.parseDouble(sc.nextLine());
-        purchases.add(new Purchase(name, price));
         balance -= price;
-        System.out.println("Purchase was added!\n");
+        return new Purchase(name, price);
     }
-    public void showPurchases() {
-        if (purchases.size() == 0) {
+
+    public void displayPurchases(String input) {
+
+        ArrayList<Purchase> category = new ArrayList<>();
+        switch (input) {
+            case "1", "2", "3", "4" -> category = purchases.getOrDefault(Integer.valueOf(input), new ArrayList<>());
+            case "5" ->
+                    category = (ArrayList<Purchase>) purchases.values().stream().flatMap(ArrayList::stream).collect(Collectors.toList());
+        }
+        if (category.size() == 0) {
             System.out.println("The purchase list is empty\n");
         } else {
-            double sum = purchases.stream().mapToDouble(Purchase::getPrice).sum();
-            purchases.forEach(System.out::println);
+            double sum = category.stream().mapToDouble(Purchase::getPrice).sum();
+            category.forEach(System.out::println);
             System.out.printf("Total sum: $%.2f\n\n", sum);
         }
     }
--- src/budget/Main.java
+++ src/budget/Main.java
@@ -5,28 +5,77 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        Budget budget = new Budget();
         Scanner sc = new Scanner(System.in);
+        Budget budget = new Budget();
         boolean toContinue = true;
-        while(toContinue) {
-            System.out.println("Choose your action:\n" +
-                    "1) Add income\n" +
-                    "2) Add purchase\n" +
-                    "3) Show list of purchases\n" +
-                    "4) Balance\n" +
-                    "0) Exit\n");
+        while (toContinue) {
+            System.out.print("""
+                    Choose your action:
+                    1) Add income
+                    2) Add purchase
+                    3) Show list of purchases
+                    4) Balance
+                    0) Exit
+                    """);
             String option = sc.nextLine();
             System.out.println();
             switch (option) {
                 case "1" -> {
                     System.out.println("Enter income:");
-                    budget.addIncome(Double.parseDouble(sc.nextLine()));}
-                case "2" -> budget.addPurchase(sc);
-                case "3" -> budget.showPurchases();
+                    budget.addIncome(Double.parseDouble(sc.nextLine()));
+                }
+                case "2" -> addingMenu(sc, budget);
+                case "3" -> displayMenu(sc, budget);
                 case "4" -> System.out.printf("Balance: $%.2f\n\n", budget.getBalance());
                 case "0" -> toContinue = false;
             }
         }
         System.out.println("Bye!");
+    }
+
+    public static void addingMenu(Scanner sc, Budget budget) {
+        boolean toContinue = true;
+        while (toContinue) {
+            System.out.print("""
+                    Choose the type of purchases
+                    1) Food
+                    2) Clothes
+                    3) Entertainment
+                    4) Other
+                    5) Back
+                    """);
+            String input = sc.nextLine();
+            switch (input) {
+                case "1", "2", "3", "4" -> budget.addPurchase(input, sc);
+                case "5" -> {
+                    toContinue = false;
+                    System.out.println();
+                }
+            }
+        }
+    }
+
+    public static void displayMenu(Scanner sc, Budget budget) {
+        boolean toContinue = true;
+        while (toContinue) {
+            System.out.println("""
+                    Choose the type of purchases
+                    1) Food
+                    2) Clothes
+                    3) Entertainment
+                    4) Other
+                    5) All
+                    6) Back
+                    """);
+
+            String input = sc.nextLine();
+            switch (input) {
+                case "1", "2", "3", "4", "5" -> budget.displayPurchases(input);
+                case "6" -> {
+                    toContinue = false;
+                    System.out.println();
+                }
+            }
+        }
     }
 }
